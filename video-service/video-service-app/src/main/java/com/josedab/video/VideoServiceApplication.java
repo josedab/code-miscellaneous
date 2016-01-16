c8d0cf44e36ff89f8227507a10890444eb6eb4bb
Jose Baena <jose@brandwatch.com>
2016-01-15 22:56:53 -0800

    Video rest repository resource created

--- video-service/video-service-app/src/main/java/com/josedab/video/VideoServiceApplication.java
+++ video-service/video-service-app/src/main/java/com/josedab/video/VideoServiceApplication.java
@@ -1,4 +1,4 @@
-package com.josedab;
+package com.josedab.video;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ video-service/video-service-app/src/main/java/com/josedab/video/domain/Video.java
@@ -0,0 +1,60 @@
+package com.josedab.video.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Video {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String title;
+    private String description;
+    private String provider;
+    private String identifier;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+}
